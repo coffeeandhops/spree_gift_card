21ad0cffdc1a9c2eade0d989721a39c94a446c85
Jason Kroll <krolly@gmail.com>
2021-03-14 16:27:58 +1000

    removed class_eval

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,14 +1,16 @@
-Spree::Order.class_eval do
+module SpreeGiftCard
+  module OrderDecorator
 
-  include Spree::Order::GiftCard
-  
-  def finalize!
-    card_items = line_items.joins(:product).where("spree_products.is_gift_card = ?", true)
-    card_items.each do |li|
-      li.gift_card.update_column(:enabled, true)
-      ::Spree::OrderMailer.gift_card_email(li.gift_card.id, self).deliver_later
+    def finalize!
+      card_items = line_items.joins(:product).where("spree_products.is_gift_card = ?", true)
+      card_items.each do |li|
+        li.gift_card.update_column(:enabled, true)
+        ::Spree::OrderMailer.gift_card_email(li.gift_card.id, self).deliver_later
+      end
+      super
     end
-    super
-  end
 
+  end
 end
+
+::Spree::Order.prepend SpreeGiftCard::OrderDecorator
