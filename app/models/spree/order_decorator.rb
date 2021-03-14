1cf2f3750276ff75b79359bad04d5094241e040b
Jason Kroll <krolly@gmail.com>
2021-03-14 16:18:54 +1000

    moved process gift card to order to allow for nil payment

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,5 +1,14 @@
 Spree::Order.class_eval do
 
   include Spree::Order::GiftCard
+  
+  def finalize!
+    card_items = line_items.joins(:product).where("spree_products.is_gift_card = ?", true)
+    card_items.each do |li|
+      li.gift_card.update_column(:enabled, true)
+      ::Spree::OrderMailer.gift_card_email(li.gift_card.id, self).deliver_later
+    end
+    super
+  end
 
 end
